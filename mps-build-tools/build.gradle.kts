495587cbb926a40fc929992a20faef6f92d072fe
Sascha Lisson <sl@slisson.de>
2022-08-11 09:46:08 +0200

    Added missing version to junit-jupiter-engine dependency

--- mps-build-tools/build.gradle.kts
+++ mps-build-tools/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
     implementation("org.zeroturnaround:zt-zip:1.14")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("commons-io:commons-io:2.11.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
