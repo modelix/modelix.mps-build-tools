7f6a95cdd3d68138132a6c6afa0d80af6c3a5ddc
Sascha Lisson <sl@q60.de>
2025-10-25 10:39:21 +0200

    feat!: migration to gradle 9

--- build-tools-gradle-test/build.gradle.kts
+++ build-tools-gradle-test/build.gradle.kts
@@ -1,14 +1,6 @@
-import jetbrains.mps.smodel.MPSModuleRepository
-
 buildscript {
     repositories {
         maven { url = uri("https://artifacts.itemis.cloud/repository/maven-mps/") }
-    }
-    dependencies {
-        classpath("com.jetbrains:mps-openapi:2021.1.4")
-        classpath("com.jetbrains:mps-core:2021.1.4")
-        classpath("com.jetbrains:mps-environment:2021.1.4")
-        classpath("com.jetbrains:mps-platform:2021.1.4")
     }
 }
 
@@ -16,7 +8,7 @@ plugins {
     base
     `maven-publish`
     id("org.modelix.mps.build-tools")
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.2.20"
 }
 
 group = "org.modelix"
@@ -51,33 +43,8 @@ mpsBuild {
         mainMethodFqName("org.modelix.buildtools.test.ListModulesKt.listModules")
         includeProjectRuntime()
     }
-
-    runMPS("runLambdaInMPS") {
-        val prefix = "jetbrains."
-        implementation {
-            val repo = MPSModuleRepository.getInstance()
-            lateinit var moduleNames: List<String?>
-            repo.modelAccess.runReadAction {
-                moduleNames = repo.modules.filter { it.moduleName?.startsWith(prefix) == true }.map { it.moduleName }
-            }
-            moduleNames.forEach { println("Module: $it") }
-        }
-    }
-
-    runMPS("runLambdaInMPSWithResult") {
-        implementation({
-            val repo = MPSModuleRepository.getInstance()
-            lateinit var moduleNames: List<String?>
-            repo.modelAccess.runReadAction {
-                moduleNames = repo.modules.map { it.moduleName }.toList()
-            }
-            moduleNames.size
-        }, {
-            println("Number of modules: $it")
-        })
-    }
 }
 
 tasks.named("test") {
-    dependsOn("listModules", "runLambdaInMPS", "runLambdaInMPSWithResult")
+    dependsOn("listModules")
 }
